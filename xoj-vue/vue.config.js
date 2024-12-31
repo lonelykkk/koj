73e82765b28791f388385a2c6b6dddb9ea82be1d
lonelykkk <2765314967@qq.com>
2024-12-31 17:38:38 +0800

    koj前端代码

--- xoj-vue/vue.config.js
+++ xoj-vue/vue.config.js
@@ -9,8 +9,6 @@ module.exports = defineConfig({
     port: 6688,
     proxy: {
       '/api': {                                //   以'/api'开头的请求会被代理进行转发
-        // target: 'http://114.55.134.193:7777',       //   要发向的后台服务器地址  如果后台服务跑在后台开发人员的机器上，就写成 `http://ip:port` 如 `http:192.168.12.213:8081`   ip为后台服务器的ip
-        // target: 'http://106.53.179.195:9001',
         target: 'http://127.0.0.1:9001',
         changeOrigin: true,
         pathRewrite: { // 地址重写
